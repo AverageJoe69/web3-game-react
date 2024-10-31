3a5cb0590b1c2f0552adbc67cfac344fe1b4cd28
AverageJoe69 <joejconway@gmail.com>
2024-10-31 16:14:35 +0000

    Add GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web3-game-react/'  // This should match your repository name
+  base: '/web3-game-react/',
 })
